f0ab7b9ab08b3c75b6a276708333c3c0850698f3
a-siu <asiu.owo@gmail.com>
2024-02-08 21:24:34 +0800

    updated the format of the config to be more friendly

--- ASIU/autoTorch/config.js
+++ ASIU/autoTorch/config.js
@@ -1,12 +1,9 @@
-module.exports = {
-    rate: 2,  // number of checks and torches placed (per second)
+const rate = 2; // Number of checks anc torches placed per second
+const depth = 0; // Max depth reached (from player's feet)
+const height = 3; // Max height reached (from player's feet)
+const reach = 4; // Number of squares extended from the centre
+const maxReach = 6; // Actual max reach of the player
+// Default Order: Top to bottom, closest to furthest
+const reverseOrder = false; // this option reverse the order
 
-    depth: 0,   // the max depth being checked, relative to the players feet (y-axis)
-    height: 3, // the max height being checked, relative to the players feet (y-axis)
-    range: 4, // how far away from a player can be checked (in blocks)
-    // Currently, actual scan volume is a cuboid with top and bottom faces as a square of side lengths "2*range-1", with total height of "height+depth"
-
-    // the default order of scanning is from top to bottom, closest to furthest
-    reverseOrder: false // this option reverse the order
-
-}
+module.exports = { rate: rate, depth: depth, height: height, reach: reach, maxReach: maxReach, reverseOrder: reverseOrder };
